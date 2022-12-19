bacf72bc5a3645a67131facfef779cb25dfdbc17
krishnabhn <105424593+krishnabhn@users.noreply.github.com>
2022-12-19 12:13:52 +0530

    Update and rename AMI.tf to sqs.yml

--- AMI.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ami_from_instance" "example" {
-  AMI_name           = "AMI_name1"
-  source_instance_id = "source_instance_id1"
-  Creation_date      = "Creation_date1"
-  Platform           = "Platform1"
-  Virtualization     = "Virtualization1"
-
-}
